2f16f9959572c78211c1971902ac0385a7f5cb44
Alvino Handiman <alvinohandiman004@gmail.com>
2022-11-30 22:40:41 -0500

    "Complete board through col conflicts"

--- src/Board.js
+++ src/Board.js
@@ -61,8 +61,54 @@
       );
     },
 
+    /*
+    Rook Placement:
+    ---------
+    Has to be placed diagonaly
+    Every new rook diagonal
+    -------------
+    Check if there are any 1's in current array and if there are any 1's in other array at cur index
+    ------------
+    N = ;
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1]
 
-/*
+    N = 3;
+    6 Methods
+    [0, 0, 1],
+    [1, 0, 0],
+    [0, 1, 0]
+
+    N = 2;
+    2 Methods
+    [0, 1],
+    [1, 0]
+
+    Queen Placement:
+    ---------
+    Move anywhere
+    There can't be any diagonal or up and down
+    ---------
+    Check if there are any 1's in current array and if there are any 1's in other array at cur index
+    Check next array index + 1 and index - 1
+    ---------
+    0 Methods
+    [0, 1],
+    [1, 0]
+
+    0 Methods
+    [1, 0, 0],
+    [0, 0, 0],
+    [0, 0, 1]
+
+    4 Methods
+    [0, 0, 1, 0],
+    [1, 0, 0, 0],
+    [0, 0, 0, 1],
+    [0, 1, 0, 0]
+    /*
          _             _     _
      ___| |_ __ _ _ __| |_  | |__   ___ _ __ ___ _
     / __| __/ _` | '__| __| | '_ \ / _ \ '__/ _ (_)
@@ -78,13 +124,38 @@
     // --------------------------------------------------------------
     //
     // test if a specific row on this board contains a conflict
+
+    /*[0, 1, 0],
+    [0, 0, 0],
+    [0, 0, 0]*/
+
+    //Check if there are any 1's in current array and if there are any 1's in other array at cur index
+
+    //Loop through each array in matrix
+      //var one-ammount = 0
+      //Loop through row/array
+        //if one found
+          //Add to one amount
+        //if one-ammount is greater then one
+          //return false
+    //return true
+
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      var pieces = 0;
+      this.get(rowIndex).forEach(function(curSquare) {
+        pieces += curSquare;
+      });
+      return pieces > 1;//index;
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      for (var x = 0; x < this.attributes.n; x++) {
+        if (this.hasRowConflictAt(x)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -93,13 +164,31 @@
     // --------------------------------------------------------------
     //
     // test if a specific column on this board contains a conflict
+
+    //Pieces = 0
+    //Use for each and loop through every key
+      //Check cur array at colIndex
+        //If 1 found add 1 to pieces
+    //return pieces > 1
+
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      var pieces = 0;
+      _.each(this.attributes, function(curRow) {
+        if (Array.isArray(curRow) === true) {
+          pieces += curRow[colIndex];
+        }
+      });
+      return pieces > 1;
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      for (var x = 0; x < this.attributes.n; x++) {
+        if (this.hasColConflictAt(x)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
